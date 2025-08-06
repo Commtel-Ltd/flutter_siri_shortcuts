a475c4b1f28ae9d9820b28ad3f66fcdce62f9fe3
Brendan Wood <woodbrendan@hotmail.com>
2025-08-06 12:09:33 +0100

    Fix issue with app delegate

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Flutter
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
@@ -9,5 +9,31 @@ import Flutter
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  override func application(
+    _ application: UIApplication,
+    continue userActivity: NSUserActivity,
+    restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+  ) -> Bool {
+    
+    if userActivity.activityType != NSUserActivityTypeBrowsingWeb {
+      if let flutterEngine = window?.rootViewController as? FlutterViewController {
+        let channel = FlutterMethodChannel(
+          name: "flutter_siri_shortcuts",
+          binaryMessenger: flutterEngine.binaryMessenger
+        )
+        
+        var data: [String: Any] = ["activityType": userActivity.activityType]
+        if let userInfo = userActivity.userInfo {
+          data["userInfo"] = userInfo
+        }
+        
+        channel.invokeMethod("onShortcutActivated", arguments: data)
+      }
+      return true
+    }
+    
+    return super.application(application, continue: userActivity, restorationHandler: restorationHandler)
   }
 }
